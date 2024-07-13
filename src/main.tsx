865631dd44662128c8a4e5d1b1386586bef7f0a6
nimeshjayasankhanj <nimeshappledevelopment@gmail.coml>
2024-07-13 12:22:51 +0530

    fix:change app file name to route

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import Routes from './routes';
 import './css/style.css';
 import './css/satoshi.css';
 import 'flatpickr/dist/flatpickr.min.css';
@@ -9,7 +9,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Routes />
     </Router>
   </React.StrictMode>,
 );
